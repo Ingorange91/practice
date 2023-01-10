f0e36ae865d84571260a5fd36afd036e5a547b73
Ingorange91 <119380751+Ingorange91@users.noreply.github.com>
2023-01-10 12:16:52 -0800

    modificacion a api 14
    modificaciones en indexDB del api integrada

--- js/14-API aplication programming interface/IndexedDB/script.js
+++ js/14-API aplication programming interface/IndexedDB/script.js
@@ -5,13 +5,14 @@ const IDBRequest=indexedDB.open("ErnestoDB",1);
 IDBRequest.addEventListener("upgradeneeded", ()=>{
     const db=IDBRequest.result;
     
-    db.createObjectStore("nombres",{
+    db.createObjectStore("nombres",{//los objetos se guardan con con nombre y noombres en Nmobreshtml 
         autoIncrement:true //Puedes utilizar KeyPath asociacion con nombre o letras como ejemplo el rfc
     });
 
 });
 
 IDBRequest.addEventListener("success", ()=>{
+    leerObjetos();
     console.log("todo salio correctamente");
 });
 
@@ -19,24 +20,35 @@ IDBRequest.addEventListener("error",()=>{
     console.log("A ocurrido un error en la base de datos");
 });
 
-
+//Boton añadir
 document.getElementById('add').addEventListener("click",()=>{
+    const ClassPosible=document.querySelector('.posible');
     let nombre=document.getElementById("nombre").value;
     if(nombre.length>0){
-        addObjetos({nombre});
-        leerObjetos();
+        if(ClassPosible!= undefined){
+            if(confirm("Hay elmentos sin guardar deseas continuar?")){
+                addObjetos({nombre});
+                leerObjetos();
+            }
+
+        } else {
+            addObjetos({nombre});
+            leerObjetos();
+        }
+        
     }
     else{alert("El campo no puede estar vacio");}
 });
 
 
+//Agregar el objeto ala DB
 const addObjetos= objeto=>{//si funciona correctamente
-    const IDBData=getIDBData("readwrite","Objetos garegados correctamente");
+    const IDBData=getIDBData("readwrite","Objetos agregados correctamente");
     IDBData.add(objeto);
     
 }
 
-
+//leerobjetos
 const leerObjetos =objeto=>{//sin funciona corractamente
     const IDBData=getIDBData("readonly");
     const cursor=IDBData.openCursor();
@@ -52,19 +64,23 @@ const leerObjetos =objeto=>{//sin funciona corractamente
     });
 }
 
+
+//modificar objetos
 const modificarObjetos=(key, objeto)=>{//si funciona correctamente
     const IDBData=getIDBData("readwrite", "objeto modificado correctamente");
     IDBData.put(objeto,key);
    
 };
 
+
+//borrar objetos
 const borrarObjetos=key=>{//funciona correctamente
     const IDBData=getIDBData("readwrite", "Objeto borrado exitosamente");
     IDBData.delete(key);
   
 };
 
-
+//
 const getIDBData=(mode, msg)=>{
     const db=IDBRequest.result;
     const IDBtransaction=db.transaction("nombres",mode);//Se otorgan el tipo de permisos
@@ -91,13 +107,36 @@ const nombresHTML=(id,name)=>{
 
     saveButton.textContent="Guardar";
     deleteButton.textContent="Eliminar";
-    h2.textContent=name.nombres;
+    h2.textContent=name.nombre;
+    h2.setAttribute("contenteditable", "true")//Para poder editar el label y realizarle modificciones
+    h2.setAttribute("spellcheck","false");
 
     options.appendChild(saveButton);
     options.appendChild(deleteButton);
 
     container.appendChild(h2);
     container.appendChild(options);
+
+    //Modificar elementos
+    h2.addEventListener("keyup", ()=>{
+        saveButton.classList.replace("imposible","posible")
+    });
+
+    saveButton.addEventListener("click",()=>{
+        if(saveButton.className=='posible'){
+            modificarObjetos(id, {nombre: h2.textContent});
+            saveButton.classList.replace("posible", "imposible")
+        }
+    });
+
+    deleteButton.addEventListener("click", ()=>{
+        borrarObjetos(id);
+        document.querySelector(".nombres").removeChild(container);
+        
+    });
+
+
+
 
     return container
 }

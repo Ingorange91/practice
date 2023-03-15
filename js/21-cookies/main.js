0a6724fc1c177eddeb11869de28e8b3d9b2a92c5
Ingorange91 <119380751+Ingorange91@users.noreply.github.com>
2023-03-15 16:06:26 -0700

    actualizacion de cookies 15 marzo

--- js/21-cookies/main.js
+++ js/21-cookies/main.js
@@ -2,6 +2,7 @@
 const newFecha=dias=>{
     let fecha = new Date();
     fecha.setTime(fecha.getTime() + dias*1000*60*60*24);
+    //fecha.setTime(fecha.getTime() + (dias*1000*60*8));
     return fecha.toUTCString();
 }
 
@@ -10,18 +11,28 @@ const crearCookies=(name, dias) =>{
     document.cookie=`${name};expires=${expires}`;
 }
 
-//crearCookies("nombre=Orange",2);
-
-const obtenerCokkie= cookiName =>{
-    let cookies=document.cookie;
-    //console.log("valor de cookies: ",cookies);
-    cookies = cookies.split(";");
-    console.log("tamaño es",cookies.length);
+//crearCookies("nombre=yugy",2); recordar que cada name o clave de cookie debe ser diferente ya que de otra forma se sobrescribiran los valores
+'use strict'
+const obtenerCookie= cookiName =>{
+  let temp="";
+  let todasCookies=document.cookie.split(";");
+  for(let i=0; i< todasCookies.length; i++){
+    //let res=todasCookies[i].trim();
+    let res=todasCookies[i].trim().split("=");
     
-    
-    for(let i =0; cookies.length > i; i++){
-        cookie=cookies[i].trim();
-        if(cookie.startsWith(cookiName)) {return cookie.split("=")[1]}
-        else {return "no hay cookies para mostrar";}
+    if(cookiName===res[0]){
+      temp=res[1];
+      if(temp!="") break;
     }
+  }
+  
+  console.log(temp);
+  return "No existen cookies con ese nombre";
+  
 }
+
+//para modificarla solo se reescribe en el metodo document.cookie("nombreexistenteCookie=value; espire valor")
+//Para eliminar cookie document.cokie="nombrecookie=value;max-age=0" se borra alos 0 segundos
+
+
+
